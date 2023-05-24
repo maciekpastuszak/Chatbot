bef59d64630c1f892b98eda9688edff1965aa00c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-24 10:05:56 +0200

    enclose code in main func

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,29 @@
 import openai
 from dotenv import dotenv_values
+import argparse
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-messages = []
-while True:
-    try:
-        user_input = input("You: ")
-        messages.append({"role": "user", "content": user_input})
-        res = openai.ChatCompletion.create(model="gpt-4", messages=messages)
 
-        messages.appen(res["choices"][0]["message"].to_dict())
-        print(res["choices"][0]["message"]["content"])
-        print("ALL MESSAGES", messages)
+def main():
+    messages = []
+    while True:
+        try:
+            user_input = input("You: ")
+            messages.append({"role": "user", "content": user_input})
+            res = openai.ChatCompletion.create(model="gpt-4", messages=messages)
 
-    except KeyboardInterrupt:
-        print("Exiting....")
-        break
+            messages.appen(res["choices"][0]["message"].to_dict())
+            print("Assistant: ", res["choices"][0]["message"]["content"])
+            print("ALL MESSAGES", messages)
 
-print(res)
+        except KeyboardInterrupt:
+            print("Exiting....")
+            break
+
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
