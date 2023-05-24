966f680b83f29398a57b34bd58e03e3004de4c36
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-24 09:10:52 +0200

    basic config

---
+++ chatbot.py
@@ -0,0 +1,12 @@
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
+
+response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages= [{"role": "user", "content": "What is the best color on earth?"}]
+)
+
+print(response)
