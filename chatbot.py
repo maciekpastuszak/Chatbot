758715aff10c85d16241d65df5910f49620e75ca
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-24 09:41:57 +0200

    add while loop

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,12 @@ openai.api_key = config["OPENAI_API_KEY"]
 
 response = openai.ChatCompletion.create(
     model="gpt-4",
-    messages= [{"role": "user", "content": "What is the best color on earth?"}]
+    messages=[{"role": "user", "content": "What is the best color on earth according to different countries?"}],
 )
 
+while True:
+    try:
+    except KeyboardInterrupt:
+        print("Exiting....")
+        break
 print(response)
