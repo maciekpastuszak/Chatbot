0a54e0dbf17447c88a6c28abd3d2264a438c7b56
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-24 09:44:20 +0200

    introduce user input as a message

--- chatbot.py
+++ chatbot.py
@@ -4,14 +4,20 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[{"role": "user", "content": "What is the best color on earth according to different countries?"}],
-)
-
 while True:
     try:
+        user_input = input("You: ")
+        res = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input,
+                }
+            ],
+        )
     except KeyboardInterrupt:
         print("Exiting....")
         break
-print(response)
+
+print(res)
